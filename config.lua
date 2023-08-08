db3fc10fbee7f237f3c0c979514e142cc312193c
maekawa_shintaro <maeshinshin@outlook.jp>
2023-08-08 14:40:06 +0900

    initial commit

---
+++ config.lua
@@ -0,0 +1,223 @@
+--[[
+ THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+ `lvim` is the global options object
+]]
+-- vim options
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.relativenumber = true
+
+-- general
+lvim.log.level = "info"
+lvim.format_on_save = {
+    enabled = true,
+    pattern = "*.lua",
+    timeout = 1000,
+}
+-- -- barbarmaps
+-- local map = vim.api.nvim_set_keymap
+-- local opts = { noremap = true, silent = true }
+--
+-- -- Move to previous/next
+-- map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+-- map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- -- Re-order to previous/next
+-- map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+-- map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- -- Goto buffer in position...
+-- map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+-- map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+-- map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+-- map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+-- map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+-- map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+-- map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+-- map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+-- map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+-- map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- -- Pin/unpin buffer
+-- map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- -- Close buffer
+-- map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+-- -- Wipeout buffer
+-- --                 :BufferWipeout
+-- -- Close commands
+-- --                 :BufferCloseAllButCurrent
+-- --                 :BufferCloseAllButPinned
+-- --                 :BufferCloseAllButCurrentOrPinned
+-- --                 :BufferCloseBuffersLeft
+-- --                 :BufferCloseBuffersRight
+-- -- Magic buffer-picking mode
+-- map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- -- Sort automatically by...
+-- map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+-- map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+-- map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+-- map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- lvim.builtin.which_key.mappings["b"] = {
+--     name = "+Buffer",
+--     n = { '<Cmd>BufferPrevious<CR>', "Next" },
+--
+-- }
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
+
+-- to disable icons and use a minimalist setup, uncomment the following
+-- lvim.use_icons = false
+
+-- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.insert_mode["jj"] = "<ESC>"
+lvim.keys.insert_mode["<C-j>"] = "<DOWN>"
+lvim.keys.insert_mode["<C-h>"] = "<LEFT>"
+lvim.keys.insert_mode["<C-k>"] = "<UP>"
+lvim.keys.insert_mode["<C-l>"] = "<RIGHT>"
+lvim.keys.insert_mode["<C-z>"] = "<ESC>ui"
+-- lvim.keys.normal_mode["<C-z>"] = "u"
+
+lvim.builtin.which_key.mappings["m"] = {
+    name = "+shortcut",
+    c = { "iint main(){<cr><cr>}<ESC>ki<Tab>", "c++ main()" },
+    j = { "a<Tab>public static void main(String[] args) {<cr><cr>}<ESC>ki<Tab>", "java main()" },
+    s = { "aSystem.out.println(j);<ESC>hhciw", "java println()" }
+}
+lvim.builtin.which_key.mappings["t"] = {
+    name = "+Terminal",
+    f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+    v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+    h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+-- -- Use which-key to add extra bindings with the leader-key prefix
+-- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
+-- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+
+-- -- Change theme settings
+-- lvim.colorscheme = "lunar"
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- Automatically install missing parsers when entering buffer
+lvim.builtin.treesitter.auto_install = true
+
+-- lvim.builtin.treesitter.ignore_install = { "haskell" }
+
+-- -- always installed on startup, useful for parsers without a strict filetype
+-- lvim.builtin.treesitter.ensure_installed = { "comment", "markdown_inline", "regex" }
+
+-- -- generic LSP settings <https://www.lunarvim.org/docs/languages#lsp-support>
+
+-- --- disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. IMPORTANT: Requires `:LvimCacheReset` to take effect
+-- ---see the full default list `:lua =lvim.lsp.automatic_configuration.skipped_servers`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+-- local opts = {} -- check the lspconfig documentation for a list of all possible options
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
+-- local formatters = require "lvim.lsp.null-ls.formatters"
+-- formatters.setup {
+--   { command = "stylua" },
+--   {
+--     command = "prettier",
+--     extra_args = { "--print-width", "100" },
+--     filetypes = { "typescript", "typescriptreact" },
+--   },
+-- }
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup {
+--   { command = "flake8", filetypes = { "python" } },
+--   {
+--     command = "shellcheck",
+--     args = { "--severity", "warning" },
+--   },
+-- }
+
+-- -- Additional Plugins <https://www.lunarvim.org/docs/configuration/plugins/example-configurations>
+-- lvim.plugins = {
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
+lvim.plugins = {
+    {
+        "tpope/vim-surround",
+        -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+        -- setup = function()
+        --  vim.o.timeoutlen = 500
+        -- end
+    },
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+                RGB = true,      -- #RGB hex codes
+                RRGGBB = true,   -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true,   -- CSS rgb() and rgba() functions
+                hsl_fn = true,   -- CSS hsl() and hsla() functions
+                css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+        end,
+    },
+
+    -- {
+    --     'romgrk/barbar.nvim',
+    --     dependencies = {
+    --         'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+    --         'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    --     },
+    --     init = function() vim.g.barbar_auto_setup = false end,
+    --     opts = {
+    --         -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    --         -- animation = true,
+    --         -- insert_at_start = true,
+    --         -- …etc.
+    --     },
+    --     version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    --     config = function()
+    --         vim.g.barbar_auto_setup = false
+    --     end
+    -- }
+}
+
+-- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
